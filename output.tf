bd5a971e731092ae8184080f72e2d9e39f041335
cklewar@t-online.de <cklewar@t-online.de>
2022-11-14 17:28:06 +0100

    v0.11.14 v10

--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
-output "azure-site-1a" {
+/*output "azure-site-1a" {
   value = module.azure-site-1a
 }
 
 output "azure-site-1b" {
   value = module.azure-site-1b
-}
+}*/
 
 output "aws-site-1a" {
   value = module.aws-site-1a
 }
 
-output "aws-site-1b" {
+/*output "aws-site-1b" {
   value = module.aws-site-1b
 }
 
@@ -20,4 +20,4 @@ output "gcp-site-1a" {
 
 output "gcp-site-1b" {
   value = module.gcp-site-1b
-}
+}*/
